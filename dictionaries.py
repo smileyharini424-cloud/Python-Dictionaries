9a6cc8cc23f5304556f76db5c76884e5774f09e2
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-18 23:53:11 -0700

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+student = {
+    "name": "Harini",
+    "age": 20,
+    "course": "CSE",
+    "city": "Hyderabad"
+}
+
+print("Student Dictionary:", student)
+
+print("Name:", student["name"])
+print("Course:", student["course"])
+
+student["college"] = "ABC College"
+print("After Adding:", student)
+
+student["age"] = 21
+print("After Updating:", student)
